5be608a4ae53467ea27af509d77a94c7262561d9
Anushrav <122109103+anushrav23@users.noreply.github.com>
2024-05-06 16:23:30 -0400

    Update streamlit_app_datastax.py

--- streamlit_app_datastax.py
+++ streamlit_app_datastax.py
@@ -288,7 +288,7 @@ def erase_history():
     
 def main():
     text=''
-    st.image('images/poweredby.jpg', width=550)
+    
     st.title("Fiddler Chatbot")
     if not st.session_state[UUID] or st.session_state[UUID] is None:
         st.session_state[UUID] = uuid_g.uuid4()
@@ -321,7 +321,7 @@ def main():
             
         st.session_state.messages.append({"role": "assistant", "content": full_response["answer"]})
         st.session_state[ANSWER] = full_response["answer"]
-        
+        st.image('images/poweredby.jpg', width=100)
         publish_and_store(full_response["question"], full_response["answer"], full_response["source_documents"], (end_time - start_time))
 
     if st.session_state[ANSWER] is not None:
@@ -350,10 +350,6 @@ def main():
         <style>
             ul.streamlit-expander {
                 border: 10 !important;}
-                
-            .container .main .block-container {{
-                    padding-bottom: {40}rem;
-                }}  
                 
         </style>
         """
