8a170b04f191d72ab2bba63d8d13311d55298ad9
Anushrav <122109103+anushrav23@users.noreply.github.com>
2024-12-23 14:19:46 -0500

    Update streamlit_app_datastax.py

--- streamlit_app_datastax.py
+++ streamlit_app_datastax.py
@@ -386,7 +386,10 @@ def main():
             st.button("Reset Chat History", on_click=erase_history)
         with col5:
             output_str = f'Answer Failthfulness:  ' + str(float("{:.3f}".format(faithfulness_score)))
-            st.markdown(output_str)
+            if faithfulness_score<0.5:
+              st.markdown(''':red[{output_str}]''')
+            else:
+              st.markdown(''':green[{output_str}]''')
             
         
         with st.expander("Click here to leave your feedback on the chatbot response"):
