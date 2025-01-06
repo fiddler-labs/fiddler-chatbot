e5c9b39cc61d4a8103779fecddac6834da883bbf
Anushrav <122109103+anushrav23@users.noreply.github.com>
2025-01-06 18:46:21 -0500

    Update streamlit_app_datastax.py

--- streamlit_app_datastax.py
+++ streamlit_app_datastax.py
@@ -115,12 +115,6 @@ non_stream_llm = ChatOpenAI(model_name=LLM_MODEL, temperature=0)
 memory = ConversationSummaryBufferMemory(llm=non_stream_llm, memory_key="chat_history", return_messages=True, max_tokens_limit=50, output_key='answer')
 question_generator = LLMChain(llm=non_stream_llm, prompt=CONDENSE_QUESTION_PROMPT)
 
-if FAITHFULNESS_SCORE not in st.session_state:
-    st.session_state[FAITHFULNESS_SCORE] = 0.0
-if JAILBREAK_SCORE not in st.session_state:
-    st.session_state[JAILBREAK_SCORE] = 0.0
-if SAFETY_GAURDRAIL_LATENCY not in st.session_state:
-    st.session_state[SAFETY_GAURDRAIL_LATENCY] = 0.0
 
 if THUMB_DOWN not in st.session_state:
     st.session_state[THUMB_DOWN] = None
@@ -395,12 +389,13 @@ def main():
         st.session_state[ANSWER] = full_response["answer"]
       
         logger.info(type(full_response["source_documents"][0]))
-        FAITHFULNESS_SCORE, faithfulness_gaurdrail_latency = get_faithfulness_gaurdrail_results(full_response["question"], full_response["answer"], full_response["source_documents"])
-        JAILBREAK_SCORE, SAFETY_GAURDRAIL_LATENCY = get_safety_gaurdrail_results(full_response["question"])
         publish_and_store(full_response["question"], full_response["answer"], full_response["source_documents"], (end_time - start_time))
+      
     if st.session_state[ANSWER] is not None:
         
         # Display thumbs up and thumbs down buttons
+        FAITHFULNESS_SCORE, faithfulness_gaurdrail_latency = get_faithfulness_gaurdrail_results(full_response["question"], full_response["answer"], full_response["source_documents"])
+        JAILBREAK_SCORE, SAFETY_GAURDRAIL_LATENCY = get_safety_gaurdrail_results(full_response["question"])
         col1, col2, col3, col4, col5, col6 = st.columns([0.5, 0.5, 3.0, 3.0, 3.0, 3.0])
         with col1:
             if not st.session_state[THUMB_UP] or st.session_state[THUMB_UP] is None:
