50ce1eed6307e62d340bb7132d4816adacff715c
saif-raja <saifuddin@fiddler.ai>
2025-07-16 19:09:34 +0530

    MAJOR : Bugfixes with Tool regsitration and successfull calling

--- src/agentic_tools/state_data_model.py
+++ src/agentic_tools/state_data_model.py
@@ -1,11 +1,12 @@
 # Define the state schema for our chatbot
-from typing import Sequence, NotRequired, Optional
+from typing import Sequence #, Any, List, Dict, NotRequired, Optional
 from typing_extensions import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
+# import operator
 
 class ChatbotState(TypedDict):
     """Enhanced state schema for the chatbot conversation"""
     messages: Annotated[Sequence[BaseMessage], add_messages]
-    retrieved_documents: NotRequired[Optional[list]]
-    retrieval_query: NotRequired[Optional[str]]
+    # messages: Annotated[list[Any], operator.add]
+
